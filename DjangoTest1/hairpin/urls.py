9e59e91561c4469a2b83da9ef1987ff98031c998
Matěj_Mlejnek <mrlagmrlag@gmail.com>
2024-01-30 10:39:12 +0100

    30.1.2024 djangotest - urls - randomnumber

--- DjangoTest1/hairpin/urls.py
+++ DjangoTest1/hairpin/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.models_list)
+    path('randomnumber/', views.models_list)
 
 ]
--- DjangoTest1/hairpin/views.py
+++ DjangoTest1/hairpin/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def models_list(request):
-    return HttpResponse('Testing of views')
+    random_number = random.randint(1, 100)
+    return HttpResponse (f"Random number: {random_number}")
